954acd72ac91c12d79d47ace07258d2d30fff112
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2018-02-14 09:07:09 +0200

    add more workflow options

--- lib/minimus.rb
+++ lib/minimus.rb
@@ -2,18 +2,23 @@ require_relative 'version'
 
 class Minimus
 
-  attr_reader :states, :initial_state, :current_state
+  TransitionError = Class.new(StandardError)
+
+  attr_reader :states, :initial_state
+  attr_accessor :current_state
 
   def initialize(*states)
     @states = states
     @initial_state = states.first
+    @current_state = initial_state
   end
 
   def move(new_state)
-    current_state = new_state
+    self.current_state = new_state
   end
 
   def move!(new_state)
-    raise
+    raise TransitionError, "unallowed transition from #{current_state} to #{new_state}"
+    self.current_state = new_state
   end
 end
--- spec/minimus_spec.rb
+++ spec/minimus_spec.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 RSpec.describe Minimus do
   it "has a version number" do
     expect(Minimus::VERSION).not_to be nil
@@ -8,5 +9,19 @@ RSpec.describe Minimus do
 
     expect(minimus.states).to match_array([:first, :second, :third])
     expect(minimus.initial_state).to eq(:first)
+  end
+
+  it "moves to a new state" do
+    minimus = described_class.new(:first, :second, :third)
+    minimus.move(:second)
+
+    expect(minimus.current_state).to eq(:second)
+  end
+
+  it "raises an error" do
+    minimus = described_class.new(:first, :second, :third)
+    expect {
+      minimus.move!(:third)
+    }.to raise_error(Minimus::TransitionError)
   end
 end
